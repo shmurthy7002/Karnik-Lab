2418ad4c061d57add1810364235174782c5910a7
shmurthy7002 <yhtrum7002@gmail.com>
2024-10-30 11:20:23 -0400

    latest commit my computer is bugging out

--- Binding_Affinity_Prediction/make_prediction.py
+++ Binding_Affinity_Prediction/make_prediction.py
@@ -1,10 +1,10 @@
 import pickle
 from PyFingerprint.fingerprint import get_fingerprint
 
-with open("model.pkl", 'rb') as file:
+with open("new_model.pkl", 'rb') as file:
     model = pickle.load(file)
 
-with open("removed_cols.pkl", 'rb') as file:
+with open("new_removed_cols.pkl", 'rb') as file:
     cols_2_remove = pickle.load(file)
 
 
@@ -43,16 +43,57 @@ def get_X(smiles):
     print(features_df.shape)
     return features_df
 
-smiles = []
-with open(filename, 'r') as file:
-    csv_reader = csv.reader(file)
-    for row in csv_reader:
-        smiles.append(row[0])
+# smiles = []
+# with open(filename, 'r') as file:
+#     csv_reader = csv.reader(file)
+#     for row in csv_reader:
+#         smiles.append(row[0])
 
-IC50s = model.predict(get_X(smiles).to_numpy())
-import json
-with open("dump", 'w') as f:
-    json.dump(IC50s.tolist(), f, indent=4)
+def remove_outliers(value):
+    if(value < 5000):
+        return value
+    return None
 
-print(IC50s)
-print(min(IC50s))
+df = pd.read_csv("ic50_mols.csv")
+df = df.dropna(subset=["IC50 (nM)"])
+df = df[pd.to_numeric(df['IC50 (nM)'], errors='coerce').notna()]
+df['IC50 (nM)'] = df['IC50 (nM)'].astype(float)
+
+df['IC50 (nM)'] = df['IC50 (nM)'].apply(remove_outliers)
+df = df.dropna(subset=['IC50 (nM)'])
+print(df.shape)
+print(df.columns)
+smiles = df['Ligand SMILES'].tolist()
+
+df['IC50_nM_float'] = pd.to_numeric(df['IC50 (nM)'], errors='coerce')
+df = df.dropna(subset=['IC50_nM_float'])
+df['IC50 (M)'] = df['IC50_nM_float'] / 1e9
+df['pIC50'] = -np.log10(df['IC50 (M)'])
+
+y_pred = model.predict(get_X(smiles).to_numpy())
+y_actual = df['pIC50'].tolist()
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 6))
+
+plt.scatter(y_actual, y_pred, label='Predicted vs True', color='blue', alpha=0.5)
+
+# Line with slope 1 going through (0, 0)
+min_val = min(min(y_actual), min(y_pred))
+max_val = max(max(y_actual), max(y_pred))
+plt.plot([min_val, max_val], [min_val, max_val], color='red', linestyle='--', label='Ideal Prediction')
+
+plt.xlabel('Actual pIC50')
+plt.ylabel('Predicted pIC50')
+plt.title('Predicted vs Actual pIC50')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+# import json
+# with open("dump", 'w') as f:
+#     json.dump(IC50s.tolist(), f, indent=4)
+
+# print(IC50s)
+# print(min(IC50s))
